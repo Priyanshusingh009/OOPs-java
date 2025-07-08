ff116b9113059a831f87d653ae28361ff58bfe53
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-07-08 12:38:22 +0530

    cylinder height and radius

---
+++ cylinder.java
@@ -0,0 +1,8 @@
+//create a class cylinder and use getter and setter to set its radius and height
+public class cylinder {
+
+    public static void main(String[] args) {
+        System.out.println("setting cylinder height and radius");
+    }
+}
+
